5fa7302084b0eb852565d5d149e3c34a0bc00eb6
bledsoef <bledsoef@berea.edu>
2022-12-19 00:15:16 -0500

    finished module 3 and created interface for prices

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 299.99, date: new Date(2021, 2, 28) },
+    { title: "TV", amount: 199.99, date: new Date(2021, 2, 28) },
+    { title: "Netflix", amount: 8.99, date: new Date(2021, 2, 28) },
+    { title: "Hulu", amount: 6.99, date: new Date(2021, 2, 28) },
+  ];
   return (
     <div>
-      <h2>Let's go!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses expenses={expenses}></Expenses>
     </div>
   );
 }
